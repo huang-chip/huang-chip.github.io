b5c0eb3d1adfeb2e645aab59add066b6d67c2c9c
huang-chip <huang-chip@users.noreply.github.com>
2025-10-22 06:19:38 +0000

    deploy: dc5acdfe96e6dd0e1b521bfe42afa963f137b3b3

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/huang-chip.github.io/sw.js', { scope: '/huang-chip.github.io/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
