1e6773710bb1908fde401cf45c5f4c9f0e4d0d66
huangxinyuan <694327101@qq.com>
2025-10-22 10:11:05 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/drtao.github.io/sw.js', { scope: '/drtao.github.io/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/huang-chip.github.io/sw.js', { scope: '/huang-chip.github.io/' })})}
